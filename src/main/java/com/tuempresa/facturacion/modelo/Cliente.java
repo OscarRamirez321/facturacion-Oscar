2c96e6cba42877a2fec7195eb50765357b94cbe3
oamar <oaramirez@uamv.edu.ni>
2025-11-03 18:03:14 -0600

    Paso 2

--- src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
+++ src/main/java/com/tuempresa/facturacion/modelo/Cliente.java
@@ -9,11 +9,32 @@ import lombok.*;
 public class Cliente {
 
     @Id
-    @Column(length=6)
+    @Column(length = 6)
     int numero;
 
-    @Column(length=50)
+    @Column(length = 50)
     @Required
     String nombre;
+
+    @Embeddable // Usamos @Embeddable en vez de @Entity
+    @Getter
+    @Setter
+    public class Direccion {
+
+        @Column(length = 30) // Los miembros se anotan igual que en las entidades
+        String viaPublica;
+
+        @Column(length = 5)
+        int codigoPostal;
+
+        @Column(length = 20)
+        String municipio;
+
+        @Column(length = 30)
+        String provincia;
+    }
+    @Embedded // Así para referenciar a una clase incrustable
+    Direccion direccion; // Una referencia Java convencional
 }
+
 
